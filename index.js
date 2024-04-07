809ccc063a48e092d82859093795b1cd84deea08
FierceSpectrum <127576883+FierceSpectrum@users.noreply.github.com>
2024-04-06 21:23:59 -0600

    JWT aggregation
    JTW was added for authentication, when using authentication the token expires due to time

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const User = require("./models/userModel");
+
 const express = require("express");
 const app = express();
 // database connection
@@ -7,6 +12,8 @@ const db = mongoose.connect("mongodb://127.0.0.1:27017/users2", {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+const theSecretKey = process.env.JWT_SECRET;
 
 const {
   userVerificad,
@@ -44,10 +51,11 @@ const { avatarCreat, avatarGet } = require("./controllers/avatarController.js");
 
 // parser for the request body (required for the POST and PUT methods)
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
-// check for cors
 const cors = require("cors");
+
+// Middlewares
+app.use(bodyParser.json());
+// check for cors
 app.use(
   cors({
     domains: "*",
@@ -55,13 +63,98 @@ app.use(
   })
 );
 
+app.post("/api/users", userPost);
+
+// login with JWT
+app.post("/api/session", function (req, res) {
+  User.find({ state: true })
+    .then((users) => {
+      const user = users.filter(
+        (user) =>
+          user.email === req.body.email && user.password === req.body.password
+      );
+      if (!user) {
+        res.status(422);
+        res.json({ error: "Invalid username or password" });
+        return;
+      }
+
+      const data = user[0];
+      const dateNow = new Date();
+      const dateAfterOneMinute = new Date(dateNow.getTime() + 60000);
+
+      const token = jwt.sign(
+        {
+          userID: data._id,
+          name: data.name,
+          permission: ["create", "edit", "delete"],
+          expiration: dateAfterOneMinute,
+          pin: data.pin,
+          phone: data.phone,
+          country: data.contry,
+          last_name: data.last_name,
+        },
+        theSecretKey
+      );
+
+      res.status(201);
+      res.json({ token, user: data });
+    })
+    .catch((err) => {
+      res.status(500);
+      res.json({ "Internal server error": err });
+    });
+});
+
+// JWT Authentication middleware
+app.use(function (req, res, next) {
+  if (req.headers["authorization"]) {
+    const authToken = req.headers["authorization"].split(" ")[1];
+    try {
+      jwt.verify(authToken, theSecretKey, (err, decodedToken) => {
+        if (err || !decodedToken) {
+          res.status(401);
+          res.json({
+            error: "Unauthorized",
+          });
+          return;
+        }
+
+        const currentDate = new Date();
+        const expirationDate = new Date(decodedToken.expiration);
+
+        if (currentDate.getTime() > expirationDate.getTime()) {
+          res.status(401);
+          res.json({
+            error: "Unauthorized",
+          });
+          return;
+        }
+        console.log("Welcome", decodedToken.name);
+        next();
+      });
+    } catch (e) {
+      console.error("There was an error", e);
+      res
+        .send({
+          error: "Unauthorized ",
+        })
+        .status(401);
+    }
+  } else {
+    res.status(401);
+    res.send({
+      error: "Unauthorized ",
+    });
+  }
+});
+
 // listen to the task request
 
 // user
 app.patch("/api/usersVerificad", userVerificad);
 app.post("/api/usersLogin", userLogin);
 app.get("/api/users", userGet);
-app.post("/api/users", userPost);
 app.patch("/api/users", userPatch);
 app.put("/api/users", userPatch);
 app.delete("/api/users", userDelete);
